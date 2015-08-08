adb380e153f8c89698911f051ca8590e73b07380
Czipperz <czipperz@gmail.com>
2015-08-07 19:34:30 -0500

    No `"` at all allowed

--- src/ParseWord.hs
+++ src/ParseWord.hs
@@ -12,9 +12,11 @@ parseWord start line = goForward . byepassWhitespace $ cropTo start line
         byepassWhitespace (x   :xs) = x:xs
 
         goForward ('\'':xs) = walkTo '\'' (xs,2)
+        goForward ('"' :xs) = error "Use of double quotes as the start of a group of words is NOT allowed"
         goForward xs        = walkTo ' '  (xs,0)
 
         walkTo _ ([]    ,n)             = ([],n)
+        walkTo _ (('"':xs),_)           = error "Use of doulbe quotes in the string body is NOT allowed"
         walkTo c ((x:xs),n) | c == x    = ([],n)
                             | otherwise = let y = walkTo c (xs,n) in
                                             (x:fst y, 1+snd y)
