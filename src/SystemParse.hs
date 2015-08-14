b9134e4b9a08e021f91d10ad39c09c227eee19e0
Czipperz <czipperz@gmail.com>
2015-08-14 00:11:26 -0500

    Made ``systemParse'' work with any non ``alnum'' ending

--- src/SystemParse.hs
+++ src/SystemParse.hs
@@ -1,4 +1,4 @@
-module SystemParse where
+module SystemParse (systemParse) where
 
 import System.Environment
 
@@ -9,18 +9,26 @@ systemParse ('$':exs) = do
   car <- getEnv var
   cdr <- systemParse ret
   return $ car++cdr
-  where parseVar ('{':xs) = parseInCurly xs
-        parseVar x        = parseNormal x
-        parseInCurly []        = ([],[])
-        parseInCurly ('}':xs)  = (xs,[])
-        parseInCurly (x  :xs)  = let (ret,cdr) = parseInCurly xs
-                                 in (ret,x:cdr)
-        parseNormal []         = ([],[])
-        parseNormal xs@(' ':_) = (xs,[])
-        parseNormal xs@('-':_) = (xs,[])
-        parseNormal xs@('/':_) = (xs,[])
-        parseNormal (x  :xs)   = let (ret,cdr) = parseNormal xs
-                                 in (ret,x:cdr)
 systemParse (x:xs) = do
   parsed <- systemParse xs
   return $ x:parsed
+
+parseVar :: String -> (String,String)
+parseVar ('{':xs) = parseInCurly xs
+parseVar x        = parseNormal False x
+
+parseInCurly :: String -> (String,String)
+parseInCurly []        = ([],[])
+parseInCurly ('}':xs)  = (xs,[])
+parseInCurly (x  :xs)  = let (ret,cdr) = parseInCurly xs in (ret,x:cdr)
+
+parseNormal ::  Bool -> String -> (String,String)
+parseNormal _ [] = ([],[])
+parseNormal True (x:xs) | x >= '0' && x <= '9' =
+  let (ret,cdr) = parseNormal True xs in
+  (ret,x:cdr)
+parseNormal _    (x:xs) | x >= 'a' && x <= 'z' ||
+                          x >= 'A' && x <= 'Z' =
+  let (ret,cdr) = parseNormal True xs in
+  (ret,x:cdr)
+parseNormal _ xs = (xs,[])
